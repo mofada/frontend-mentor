a176c266410d3fb237689dcbd1e1df9f8f832a75
fada <fada@mofada.cn>
2024-08-09 11:51:11 +0800

    1. prepare start time-tracking-dashboard

---
+++ challenges/time-tracking-dashboard/tailwindcss/tailwind-config.js
@@ -0,0 +1,23 @@
+import "./tailwindcss-3.4.5.min.js"
+
+tailwind.config = {
+    theme: {
+        colors: {
+            blue: 'hsl(246, 80%, 60%)',
+            'light-red-work': 'hsl(15, 100%, 70%)',
+            'soft-blue-play': 'hsl(195, 74%, 62%)',
+            'light-red-study': 'hsl(348, 100%, 68%)',
+            'lime-green-exercise': 'hsl(145, 58%, 55%)',
+            violet: 'hsl(264, 64%, 52%)',
+            'soft-orange-self-care': 'hsl(43, 84%, 65%)',
+            'very-dark-blue': 'hsl(226, 43%, 10%)',
+            'dark-blue': 'hsl(235, 46%, 20%)',
+            'desaturated-blue': 'hsl(235, 45%, 61%)',
+            'pale-blue': 'hsl(236, 100%, 87%)',
+            white: 'hsl(0, 0%, 100%)',
+        },
+        fontFamily: {
+            rubik: ["Rubik", 'sans-serif'],
+        }
+    }
+}
