1ee34dc46bb484dddf1a216e696ac613591d5e6b
fada <fada@mofada.cn>
2024-08-10 23:23:34 +0800

    1. product-preview-card-component: complete some part

--- challenges/time-tracking-dashboard/tailwindcss/tailwind-config.js
+++ challenges/time-tracking-dashboard/tailwindcss/tailwind-config.js
@@ -4,12 +4,12 @@ tailwind.config = {
     theme: {
         colors: {
             blue: 'hsl(246, 80%, 60%)',
-            'light-red-work': 'hsl(15, 100%, 70%)',
-            'soft-blue-play': 'hsl(195, 74%, 62%)',
-            'light-red-study': 'hsl(348, 100%, 68%)',
-            'lime-green-exercise': 'hsl(145, 58%, 55%)',
-            violet: 'hsl(264, 64%, 52%)',
-            'soft-orange-self-care': 'hsl(43, 84%, 65%)',
+            work: 'hsl(15, 100%, 70%)',
+            play: 'hsl(195, 74%, 62%)',
+            study: 'hsl(348, 100%, 68%)',
+            exercise: 'hsl(145, 58%, 55%)',
+            social: 'hsl(264, 64%, 52%)',
+            'self-care': 'hsl(43, 84%, 65%)',
             'very-dark-blue': 'hsl(226, 43%, 10%)',
             'dark-blue': 'hsl(235, 46%, 20%)',
             'desaturated-blue': 'hsl(235, 45%, 61%)',
