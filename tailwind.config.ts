fae8036ae913010faa62a5c75147baa47d9017dc
fada <fada@mofada.cn>
2024-08-27 18:50:50 +0800

    1. developing

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+// webstorm autocomplete fix
+
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
