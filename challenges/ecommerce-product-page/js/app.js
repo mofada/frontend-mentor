9efe23f5b6130e06d8f4141e4edd5e82bc77d7b5
fada <fada@mofada.cn>
2024-09-03 22:42:51 +0800

    1. ecommerce-product-page complete 85%

--- challenges/ecommerce-product-page/js/app.js
+++ challenges/ecommerce-product-page/js/app.js
@@ -1,34 +1,32 @@
 const App = {
     /**
-     * @type {HTMLButtonElement}
+     * count
      */
-    previousButton: null,
-    /**
-     * @type {HTMLButtonElement}
-     */
-    nextButton: null,
-    /**
-     * @type {HTMLElement}
-     */
-    shopCart: null,
-    /**
-     * @type {HTMLElement}
-     */
-    shopCartContainer: null,
+    currentPicture: 1,
 
-    init() {
-        this.previousButton = document.querySelector('#previous')
-        this.nextButton = document.querySelector('#next')
-        this.shopCart = document.querySelector('.shop-cart')
-        this.shopCartContainer = document.querySelector('.shop-cart-container')
+    /**
+     * init thumbnail
+     */
+    initThumbnail() {
+        const thumbnailContainer = document.querySelector('.thumbnail-container');
+        const mainImage = document.querySelector('.main-image');
 
-        // check if elements exist
-        console.assert(this.previousButton !== null, 'previousButton element not found')
-        console.assert(this.nextButton !== null, 'nextButton element not found')
-        console.assert(this.shopCart !== null, 'shopCart element not found')
-        console.assert(this.shopCartContainer !== null, 'shopCartContainer element not found')
+        thumbnailContainer.querySelectorAll('input[type="radio"]')
+            .forEach((thumbnail) => {
+                thumbnail.addEventListener('click', (event) => {
+                    mainImage.src = thumbnail.value;
+                })
+            })
+    },
+    /**
+     * init shop cart
+     */
+    initShopCart() {
+        const shopCart = document.querySelector('.shop-cart')
+        const shopCartContainer = document.querySelector('.shop-cart-container')
 
-        this.shopCart.addEventListener('click', (event) => {
+        // show shop cart container
+        shopCart.addEventListener('click', (event) => {
             const target = event.currentTarget.parentElement;
 
             // set position
@@ -39,27 +37,112 @@ const App = {
 
             const right = Math.max(windowWidth - x, 8);
 
-            this.shopCartContainer.style.right = `${right}px`;
-            this.shopCartContainer.style.top = `${y}px`;
+            shopCartContainer.style.right = `${right}px`;
+            shopCartContainer.style.top = `${y}px`;
 
             // toggle shop cart
-            this.shopCartContainer.classList.toggle('hidden')
-            this.shopCartContainer.classList.toggle('flex')
+            shopCartContainer.classList.toggle('hidden')
+            shopCartContainer.classList.toggle('flex')
+        })
+    },
+    /**
+     * init quantity
+     */
+    initQuantity() {
+        const quantity = document.querySelector('#quantity')
+        const minus = document.querySelector('#minus')
+        const plus = document.querySelector('#plus')
+        const addToCart = document.querySelector('#add-to-cart')
+
+        minus.addEventListener('click', () => {
+            const value = parseInt(quantity.value) || 0
+            if (value > 1) {
+                quantity.value = value - 1
+            }
         })
 
-        // hide shop cart container
-        this.previousButton.addEventListener('click', () => {
-            this.shopCartContainer.classList.toggle('hidden')
+        plus.addEventListener('click', () => {
+            const value = parseInt(quantity.value) || 0
+            quantity.value = value + 1
         })
-        this.nextButton.addEventListener('click', () => {
-            this.shopCartContainer.classList.toggle('hidden')
+
+        addToCart.addEventListener('click', () => {
+            this.updateShopCart()
         })
-    }
+    },
+    /**
+     * update shop cart
+     */
+    updateShopCart() {
+        const quantity = document.querySelector('#quantity')
+        const count = quantity.value
 
+        const shopCount = document.querySelector('#shop-count')
+        shopCount.textContent = count
+        shopCount.classList.toggle('hidden', count <= 0)
 
+        const shopCartList = document.querySelector('#card-list')
+        if (count > 0) {
+            shopCartList.innerHTML = `<section
+		class="self-stretch h-[50px] grid grid-cols-[auto,1fr,auto] grid-rows-[auto,auto] gap-x-4">
+	<img src="images/image-product-1-thumbnail.jpg" alt=""
+	     class="w-[50px] h-[50px] row-span-2 rounded"/>
+
+	<h3 class="row-span-1 text-blue">
+		Fall Limited Edition Sneakers
+	</h3>
+
+	<button class="row-span-2 self-center" onclick="App.removeShop()">
+		<img src="images/icon-delete.svg" alt="delete icon"/>
+	</button>
+
+	<p class="text-[16px] text-blue">$125.00 x ${count}
+		<span class="text-blue-900 font-bold ml-[5px] text-[17px]">${(125 * count).toFixed(2)}</span>
+	</p>
+</section>
+
+<button class="bg-orange text-blue-900 rounded-lg px-4 py-2 h-[52px] self-stretch font-bold">
+	Checkout
+</button>
+`
+        } else {
+            shopCartList.innerHTML = '<span class="font-bold m-auto">Your cart is empty.</span>'
+        }
+    },
+    removeShop() {
+        const quantity = document.querySelector('#quantity')
+        quantity.value = ''
+
+        this.updateShopCart()
+    },
+    initPicture() {
+        const banner = document.querySelector('#banner')
+
+        const previousButton = document.querySelector('#previous')
+        const nextButton = document.querySelector('#next')
+
+        previousButton.addEventListener('click', () => {
+            this.currentPicture -= 1
+            updatePicture(this.currentPicture)
+        })
+
+        nextButton.addEventListener('click', () => {
+            this.currentPicture += 1
+            updatePicture(this.currentPicture)
+        })
+
+        function updatePicture(index) {
+            previousButton.disabled = index <= 1
+            nextButton.disabled = index >= 4
+            banner.src = `images/image-product-${index}.jpg`
+        }
+    },
 }
 
 window.addEventListener('load', () => {
-    App.init()
+    App.initThumbnail()
+    App.initShopCart()
+    App.initQuantity()
+    App.initPicture()
     console.log('App is running...')
 })
